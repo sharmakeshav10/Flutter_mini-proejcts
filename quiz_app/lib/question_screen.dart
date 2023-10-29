52dc5f00d7613a5b247b4f22886849f0f6d99434
Keshav Sharma <sharmakeshav0708@gmail.com>
2023-10-29 16:07:15 +0530

    managing the questions as a state

--- quiz_app/lib/question_screen.dart
+++ quiz_app/lib/question_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -12,9 +13,17 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
-  var currentQuestion = questions[0];
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex += 1;
+    });
+  }
+
   @override
   Widget build(context) {
+    var currentQuestion = questions[currentQuestionIndex];
     return Container(
       margin: EdgeInsets.all(60),
       child: Center(
@@ -24,7 +33,10 @@ class _QuestionScreenState extends State<QuestionScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: TextStyle(color: Colors.white),
+              style: GoogleFonts.nunito(
+                  color: Color.fromARGB(255, 216, 228, 238),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
             const SizedBox(
@@ -33,7 +45,7 @@ class _QuestionScreenState extends State<QuestionScreen> {
             //getShuffled will shuffle our list on every reload but our original list is not mutated
             ...currentQuestion.getShuffledAnswers().map(
               (answer) {
-                return AnswerButton(answerText: answer, onTap: () {});
+                return AnswerButton(answerText: answer, onTap: answerQuestion);
               },
             ),
           ],
