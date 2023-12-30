7b69be6a6d18f83874b8775a1160c52b4b630d49
Keshav Sharma <sharmakeshav0708@gmail.com>
2023-12-30 16:12:24 +0530

    Added take/add picture functionality to the image input widget

--- favorite_places/lib/widgets/image_input.dart
+++ favorite_places/lib/widgets/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   ImageInput({super.key});
@@ -10,19 +13,49 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? selectedImage;
+
+  void takePicture() async {
+    final picker = ImagePicker();
+    final pickedImage =
+        await picker.pickImage(source: ImageSource.camera, maxWidth: 600);
+
+    if (pickedImage == null) {
+      return;
+    }
+
+    setState(() {
+      selectedImage = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+      icon: Icon(Icons.camera),
+      onPressed: takePicture,
+      label: Text('Take a picture'),
+    );
+
+    if (selectedImage != null) {
+      content = GestureDetector(
+        onTap: takePicture,
+        child: Image.file(
+          selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
+
     return Container(
       decoration: BoxDecoration(
           border: Border.all(
               width: 1, color: Theme.of(context).colorScheme.primary)),
       height: 200,
       width: double.infinity,
-      child: TextButton.icon(
-        icon: Icon(Icons.camera),
-        onPressed: () {},
-        label: Text('Take a picture'),
-      ),
+      child: content,
     );
   }
 }
